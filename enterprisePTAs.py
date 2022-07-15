da5973845b51a66c86b89fea4dcd8d687f14b510
Kyle Gersbach <gersbach.ka@gmail.com>
2022-07-15 13:43:12 -0500

    fixed model generation

--- enterprisePTAs.py
+++ enterprisePTAs.py
@@ -14,6 +14,7 @@ from enterprise.signals import gp_priors
 
 from enterprise_extensions import model_utils
 
+from PTMCMCSampler.PTMCMCSampler import PTSampler as ptmcmc
 
 def crnPTA(psrs,fixedGamma=True):
     Tspan = model_utils.get_tspan(psrs)
@@ -37,3 +38,22 @@ def crnPTA(psrs,fixedGamma=True):
     # initialize PTA
     pta = signal_base.PTA([model(psr) for psr in psrs])
     return pta
+
+def mcSample(pta,n=1000000,outDir='./PerFreq2A/',resume=False):
+    # dimension of parameter space
+    ndim = len(pta.param_names)
+
+    # initial jump covariance matrix
+    cov = np.diag(np.ones(ndim) * 0.01**2)
+
+    # set up jump groups by red noise groups 
+    groups  = [range(0, ndim)] #CHECK
+
+    # intialize sampler
+    sampler = ptmcmc(ndim, pta.get_lnlikelihood, pta.get_lnprior, cov, groups=groups,
+                     outDir=outDir,resume=False)
+
+    # sampler for N steps
+    x0 = np.hstack(p.sample() for p in pta.params)
+    
+    sampler.sample(x0, n, SCAMweight=30, AMweight=15, DEweight=50)
